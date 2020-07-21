33e238b9d44b9d65c13d0fcb8e890a75988c9c99
kugesh-Rajasekaran <47845948+kugesh-Rajasekaran@users.noreply.github.com>
2020-07-21 20:35:05 +0530

    Updates

---
+++ precache-manifest.c9012d319cf7c93159ddb67d2c29040e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521e3a57cfd3ca9a256ed7fa0fe47840",
+    "url": "/Shazaam-React/index.html"
+  },
+  {
+    "revision": "87ca2308559f5410048d",
+    "url": "/Shazaam-React/static/css/main.7828ff43.chunk.css"
+  },
+  {
+    "revision": "daf4adabe155e4efd232",
+    "url": "/Shazaam-React/static/js/2.6106b5c5.chunk.js"
+  },
+  {
+    "revision": "87ca2308559f5410048d",
+    "url": "/Shazaam-React/static/js/main.afb7426c.chunk.js"
+  },
+  {
+    "revision": "e5259e754c69d9c08db6",
+    "url": "/Shazaam-React/static/js/runtime~main.f8fdf49e.js"
+  },
+  {
+    "revision": "dd2f1ea78ed61b1ff25e4caccfd24aaf",
+    "url": "/Shazaam-React/static/media/model-pic.dd2f1ea7.jpg"
+  }
+]);
